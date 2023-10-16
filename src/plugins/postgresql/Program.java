1bf8e3c5ef879d3f592384855eab64756fd7bbc2
Josef Holubec <josefholubec@Josefs-MacBook-Air.local>
2023-10-16 12:37:26 +0200

    Separate starting of postgresql plugin updated

--- src/plugins/postgresql/Program.java
+++ src/plugins/postgresql/Program.java
@@ -1,8 +1,12 @@
 package src.plugins.postgresql;
 
+import src.plugins.postgresql.Executor;
+
 class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Executor e = new Executor("jdbc:postgresql://localhost:5432/josefholubec");
+
+        e.executeQuery("select * from person");
     }
 }
