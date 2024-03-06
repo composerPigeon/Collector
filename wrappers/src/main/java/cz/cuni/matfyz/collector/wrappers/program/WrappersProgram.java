4e077ec17e6f403e52059cca123c75e80215f662
Josef Holubec <josefholubec@Josefs-MacBook-Air.local>
2024-03-06 13:39:48 +0100

    New version of Testing program for wrappers

--- wrappers/src/main/java/cz/cuni/matfyz/collector/wrappers/program/WrappersProgram.java
+++ wrappers/src/main/java/cz/cuni/matfyz/collector/wrappers/program/WrappersProgram.java
@@ -2,6 +2,8 @@ package cz.cuni.matfyz.collector.wrappers.program;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import cz.cuni.matfyz.collector.model.DataModel;
+import cz.cuni.matfyz.collector.wrappers.abstractwrapper.WrapperException;
+import cz.cuni.matfyz.collector.wrappers.neo4j.Neo4jWrapper;
 import cz.cuni.matfyz.collector.wrappers.postgresql.PostgresWrapper;
 
 import java.sql.SQLException;
@@ -9,14 +11,26 @@ import java.sql.SQLException;
 public class WrappersProgram {
     public static void main(String[] args) {
         try {
-            PostgresWrapper wrapper = new PostgresWrapper("jdbc:postgresql://localhost:5432", "josefholubec");
+            //localhost:27017
 
-            DataModel model = wrapper.executeQuery("select * from knows_view");
+            Neo4jWrapper neo4jWrapper = new Neo4jWrapper("jdbc:neo4j:bolt://localhost:7687", "neo4j");
 
-            System.out.println(model.toJson());
+            DataModel neo4jModel = neo4jWrapper.executeQuery("MATCH (n:Train) return n");
+
+            System.out.println(neo4jModel.toJson());
+
+            //try (Neo4JConnection nativeConnection = new Neo4JConnection("bolt://localhost:7687/neo4j", "neo4j", "MiGWwErj5UxFfac")) {
+            //    System.out.println(nativeConnection.executeQueryWithExplain("MATCH (n:Train) return n"));
+            //}
+
+            PostgresWrapper postgresWrapper = new PostgresWrapper("jdbc:postgresql://localhost:5432", "josefholubec");
+
+            DataModel postgresModel = postgresWrapper.executeQuery("select * from knows_view");
+
+            System.out.println(postgresModel.toJson());
         }
-        catch (SQLException | JsonProcessingException e) {
-            System.err.println(e.getMessage());
+        catch (WrapperException | JsonProcessingException e) {
+            e.printStackTrace();
         }
 
     }
